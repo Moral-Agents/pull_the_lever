c33925dadfd9d61b72fe1a8a01e5ea8d951e0d77
gabrielvm98 <gabriel_vidal_m@hotmail.com>
2021-05-03 08:36:24 -0500

    Update PreguntaService

--- Backend/src/main/java/com/game/services/PreguntaService.java
+++ Backend/src/main/java/com/game/services/PreguntaService.java
@@ -4,6 +4,9 @@ import com.game.dtos.CreatePreguntaDto;
 import com.game.dtos.PreguntaDto;
 import com.game.exceptions.GameException;
 
+import java.util.List;
+
 public interface PreguntaService {
     PreguntaDto createPregunta(CreatePreguntaDto createPreguntaDto) throws GameException;
+    List<PreguntaDto> getPreguntas() throws GameException;
 }
--- Backend/src/main/java/com/game/services/impl/PreguntaServiceImpl.java
+++ Backend/src/main/java/com/game/services/impl/PreguntaServiceImpl.java
@@ -13,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PreguntaServiceImpl implements PreguntaService {
@@ -21,6 +23,8 @@ public class PreguntaServiceImpl implements PreguntaService {
 
     @Autowired
     private PreguntaRepository preguntaRepository;
+
+
 
     @Transactional
     @Override
@@ -37,6 +41,13 @@ public class PreguntaServiceImpl implements PreguntaService {
         }
         return modelMapper.map(getPreguntaEntity(pregunta.getId()), PreguntaDto.class);
     }
+
+    @Override
+    public List<PreguntaDto> getPreguntas() throws GameException {
+        List<Pregunta> preguntasEntity = preguntaRepository.findAll();
+        return preguntasEntity.stream().map(pregunta->modelMapper.map(pregunta,PreguntaDto.class)).collect(Collectors.toList());
+    }
+
 
     private Pregunta getPreguntaEntity(Long preguntaID) throws GameException{
         return preguntaRepository.findById(preguntaID)
