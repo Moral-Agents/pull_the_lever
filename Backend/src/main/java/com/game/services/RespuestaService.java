c7fd34658a2360e80c411fb9e2b22fe013f77130
WilmarTarazona <alonso_tch@hotmail.com>
2021-05-03 01:14:38 -0500

    Update RespuestaService.java

--- Backend/src/main/java/com/game/services/RespuestaService.java
+++ Backend/src/main/java/com/game/services/RespuestaService.java
@@ -1,4 +1,12 @@
 package com.game.services;
 
+import com.game.dtos.CreateRespuestaDto;
+import com.game.dtos.RespuestaDto;
+import com.game.exceptions.GameException;
+
+import java.util.List;
+
 public interface RespuestaService {
+    RespuestaDto createRespuesta(CreateRespuestaDto createRespuestaDto) throws GameException;
+    List<RespuestaDto> getRespuestaByPreguntaId(Long preguntaId) throws GameException;
 }
