6ce6a50288f97dadf76365814042e0c2a8d67c94
WilmarTarazona <alonso_tch@hotmail.com>
2021-05-02 22:52:05 -0500

    Implementacion createPregunta

--- Backend/src/main/java/com/game/services/impl/PreguntaServiceImpl.java
+++ Backend/src/main/java/com/game/services/impl/PreguntaServiceImpl.java
@@ -1,28 +1,45 @@
 package com.game.services.impl;
 
+import com.game.dtos.CreatePreguntaDto;
 import com.game.dtos.PreguntaDto;
 import com.game.entities.Pregunta;
 import com.game.exceptions.GameException;
+import com.game.exceptions.InternalServerErrorException;
+import com.game.exceptions.NotFoundException;
 import com.game.repositories.PreguntaRepository;
 import com.game.services.PreguntaService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import javax.transaction.Transactional;
 
 @Service
 public class PreguntaServiceImpl implements PreguntaService {
 
+    private static final ModelMapper modelMapper = new ModelMapper();
+
     @Autowired
     private PreguntaRepository preguntaRepository;
 
-    private static final ModelMapper modelMapper = new ModelMapper();
-
+    @Transactional
     @Override
-    public List<PreguntaDto> getPreguntas() throws GameException {
-        List<Pregunta> preguntasEntity = preguntaRepository.findAll();
-        return preguntasEntity.stream().map(pregunta->modelMapper.map(pregunta,PreguntaDto.class)).collect(Collectors.toList());
+    public PreguntaDto createPregunta(CreatePreguntaDto createPreguntaDto) throws GameException{
+        Pregunta pregunta = new Pregunta();
+        pregunta.setNombre(createPreguntaDto.getNombre());
+        pregunta.setImg(createPreguntaDto.getImg());
+        pregunta.setCant_si(createPreguntaDto.getCant_si());
+        pregunta.setCant_no(createPreguntaDto.getCant_no());
+        try {
+            pregunta = preguntaRepository.save(pregunta);
+        }catch (Exception ex){
+            throw new InternalServerErrorException("INTERNAL_SERVER_ERROR","INTERNAL_SERVER_ERROR");
+        }
+        return modelMapper.map(getPreguntaEntity(pregunta.getId()), PreguntaDto.class);
+    }
+
+    private Pregunta getPreguntaEntity(Long preguntaID) throws GameException{
+        return preguntaRepository.findById(preguntaID)
+                .orElseThrow(() -> new NotFoundException("NOT FOUND-404","PREGUNTA_NOTFOUND-404"));
     }
 }
