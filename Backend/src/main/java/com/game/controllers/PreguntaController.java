1f89d1ac3e977d66f0cbf50d4f6517512b66c4fb
gabrielvm98 <gabriel_vidal_m@hotmail.com>
2021-05-03 08:40:21 -0500

    Update PreguntaController.java

--- Backend/src/main/java/com/game/controllers/PreguntaController.java
+++ Backend/src/main/java/com/game/controllers/PreguntaController.java
@@ -1,15 +1,15 @@
 package com.game.controllers;
 
+import com.game.dtos.CreatePreguntaDto;
+import com.game.dtos.CreateUsuarioDto;
 import com.game.dtos.PreguntaDto;
+import com.game.dtos.UsuarioDto;
 import com.game.exceptions.GameException;
 import com.game.responses.GameResponse;
 import com.game.services.PreguntaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +24,12 @@ public class PreguntaController {
     @GetMapping("/preguntas")
     public GameResponse<List<PreguntaDto>> getPreguntas() throws GameException {
         return new GameResponse<>("Success",String.valueOf(HttpStatus.OK),"OKAY", preguntaService.getPreguntas());
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PostMapping("/preguntas")
+    public GameResponse<PreguntaDto> createPregunta(@RequestBody CreatePreguntaDto createPreguntaDto) throws GameException {
+        return new GameResponse<>("Success",String.valueOf(HttpStatus.OK), "OK",
+                preguntaService.createPregunta(createPreguntaDto));
     }
 }
