c2fc7d0b3f65dc51bdc5449f9f599c9f44311d89
Marco Altamirano <maaltamiranohiga@gmail.com>
2021-06-14 14:37:11 -0500

    Enrutamiento y correción de pequeños detalles de la landing

--- Front/pullTheLever/src/app/app-routing.module.ts
+++ Front/pullTheLever/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path:'register',
+    component:RegisterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
