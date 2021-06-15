3dc9eda631033e40c8d97c231e58db7ada80948a
WilmarTarazona <alonso_tch@hotmail.com>
2021-06-15 08:54:18 -0500

    Create GET Request

--- Front/pullTheLever/src/app/app.module.ts
+++ Front/pullTheLever/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { GameComponent } from './game/game.component';
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { GameComponent } from './game/game.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
--- Front/pullTheLever/src/app/game/game.component.ts
+++ Front/pullTheLever/src/app/game/game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RestService } from "../rest.service";
 
 @Component({
   selector: 'app-game',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
-  constructor() { }
+  constructor(private RestService:RestService) { }
 
   ngOnInit(): void {
+    // Si hay error de control access habilitar CORS
+    this.readComentarios();
   }
 
+  public readComentarios() {
+    this.RestService.get('http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true')
+      .subscribe(response => {
+        console.log(response);
+      })
+  }
 }
