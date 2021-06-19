3e722f147c8fa4f883ba7b59a37f30d052b047ca
Stephano <stephanoml@hotmail.com>
2021-06-19 11:01:21 -0500

    Atributo Fecha en Comentario creado

--- Backend/src/main/java/com/game/dtos/CreateComentarioDto.java
+++ Backend/src/main/java/com/game/dtos/CreateComentarioDto.java
@@ -3,10 +3,13 @@ package com.game.dtos;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 public class CreateComentarioDto {
     private String comentario;
+    private LocalDateTime fecha_creacion;
     private Long preguntaId;
     private Long usuarioId;
 }
